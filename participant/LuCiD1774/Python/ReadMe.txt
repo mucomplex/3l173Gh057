5a1fe2acf0d10c9e9c30887a44f87dceaaaa14d6
LuCiD1774 <sitinurnazhifahjm@gmail.com>
2018-07-23 03:03:54 +0800

    Add files via upload

---
+++ participant/LuCiD1774/Python/ReadMe.txt
@@ -0,0 +1,26 @@
+Fyi, the .ipynb is python file code that i created & run in jupyter notebook
+(quite pain in the a** to use windows & python, I used anaconda to ensure my 
+complex packages are complete whenever I'm using Windows----Linux users--congrats)
+
+.py as well known is the usual python file.
+
+
+Enjoy! ??
+
+
+What this does:
+1- Show your Mac ADDRESS
+
+2- change your mac address randomly
+
+3- change your mac address custumly
+
+4- reset the origin Mac Address
+
+5- give you reasons to change your mac address
+
+
+--I just started to write these kind of things, lemme know if something's wrong
+so I could fix it.
+
+Thanks!
---
+++ participant/LuCiD1774/Python/macchanger.py
@@ -0,0 +1,222 @@
+#
+# =======================================+SCRIPT BY LuCiD1774+==============================================
+#=============================================================================================================
+import sys
+import os
+import time
+print('''
+XX   MMMMMMMMMMMMMMMMss'''                          '''ssMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMyy''                                    ''yyMMMMMMMMMMMM   XX
+XX   MMMMMMMMyy''                                            ''yyMMMMMMMM   XX
+XX   MMMMMy''                                                    ''yMMMMM   XX
+XX   MMMy'                                                          'yMMM   XX
+XX   Mh'                                                              'hM   XX
+XX   -                                                                  -   XX
+XX                                                                          XX
+XX   ::                                                                ::   XX
+XX   MMhh.        ..hhhhhh..                      ..hhhhhh..        .hhMM   XX
+XX   MMMMMh   ..hhMMMMMMMMMMhh.                .hhMMMMMMMMMMhh..   hMMMMM   XX
+XX   ---MMM .hMMMMdd:::dMMMMMMMhh..        ..hhMMMMMMMd:::ddMMMMh. MMM---   XX
+XX   MMMMMM MMmm''      'mmMMMMMMMMyy.  .yyMMMMMMMMmm'      ''mmMM MMMMMM   XX
+XX   ---mMM ''             'mmMMMMMMMM  MMMMMMMMmm'             '' MMm---   XX
+XX   yyyym'    .              'mMMMMm'  'mMMMMm'              .    'myyyy   XX
+XX   mm''    .y'     ..yyyyy..  ''''      ''''  ..yyyyy..     'y.    ''mm   XX
+XX           MN    .sMMMMMMMMMss.   .    .   .ssMMMMMMMMMs.    NM           XX
+XX           N`    MMMMMMMMMMMMMN   M    M   NMMMMMMMMMMMMM    `N           XX
+XX            +  .sMNNNNNMMMMMN+   `N    N`   +NMMMMMNNNNNMs.  +            XX
+XX              o+++     ++++Mo    M      M    oM++++     +++o              XX
+XX                                oo      oo                                XX
+XX           oM                 oo          oo                 Mo           XX
+XX         oMMo                M              M                oMMo         XX
+XX       +MMMM                 s              s                 MMMM+       XX
+XX      +MMMMM+            +++NNNN+        +NNNN+++            +MMMMM+      XX
+XX     +MMMMMMM+       ++NNMMMMMMMMN+    +NMMMMMMMMNN++       +MMMMMMM+     XX
+XX     MMMMMMMMMNN+++NNMMMMMMMMMMMMMMNNNNMMMMMMMMMMMMMMNN+++NNMMMMMMMMM     XX
+XX     yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy     XX
+XX   m  yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy  m   XX
+XX   MMm yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy mMM   XX
+XX   MMMm .yyMMMMMMMMMMMMMMMM     MMMMMMMMMM     MMMMMMMMMMMMMMMMyy. mMMM   XX
+XX   MMMMd   ''''hhhhh       odddo          obbbo        hhhh''''   dMMMM   XX
+XX   MMMMMd             'hMMMMMMMMMMddddddMMMMMMMMMMh'             dMMMMM   XX
+XX   MMMMMMd              'hMMMMMMMMMMMMMMMMMMMMMMh'              dMMMMMM   XX
+XX   MMMMMMM-               ''ddMMMMMMMMMMMMMMdd''               -MMMMMMM   XX
+XX   MMMMMMMM                   '::dddddddd::'                   MMMMMMMM   XX
+XX   MMMMMMMM-                                                  -MMMMMMMM   XX
+XX   MMMMMMMMM                                                  MMMMMMMMM   XX
+XX   MMMMMMMMMy                                                yMMMMMMMMM   XX
+XX   MMMMMMMMMMy.                                            .yMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMy.                                        .yMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMy.                                    .yMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMs.                                .sMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMMMss.           ....           .ssMMMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMMMMMNo         oNNNNo         oNMMMMMMMMMMMMMMMMMMMM   XX
+''')
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(10. / 100)
+slowprint("[!] Starting : ")
+time.sleep(5)
+os.system('clear')
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(4. / 100)
+slowprint("    \033[91mThe Easy")
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0. / 100)
+slowprint('''\033[1;31m \033[91m    
+       __  ___                 ________                               
+      /  |/  /___ ______      / ____/ /_  ____ _____  ____ ____  _____
+     / /|_/ / __ `/ ___/_____/ /   / __ \/ __ `/ __ \/ __ `/ _ \/ ___/
+    / /  / / /_/ / /__/_____/ /___/ / / / /_/ / / / / /_/ /  __/ /    
+   /_/  /_/\__,_/\___/      \____/_/ /_/\__,_/_/ /_/\__, /\___/_/     
+                                                   /____/\033[97m             
+''')
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(3. / 100)
+slowprint("\t\t                                         \033[93mBy :LuCiD1774\033[97m")
+print(" ")
+print("1- show the current MAC-ADDRESS")
+print("")
+print("2- change your MAC-ADDRESS Randomly")
+print("")
+print("3- change your MAC-ADDRESS Customly")
+print("")
+print("4- reset the original MAC-ADDRESS")
+print(" ")
+print("5- why change MAC-ADDRESS")
+print("")
+L=input("\033[92m[?] \033[96mmake your choise ==>")
+if L==('1') :
+            print(" ")
+            print("\033[94m1 \033[97m- \033[91mwlan0 \033[97m( WIFI connexion)")
+            print("\033[94m2 \033[97m- \033[91meth0 \033[97m( CABLE connection)")
+            print(" ")
+            u=input("   \033[95m[?] \033[97menter your \033[91mconnection type \033[97m:")
+            if u==('1') :
+                      slowprint("\033[97m")
+                      os.system('macchanger -s wlan0')
+                      print(" ")
+                      alla=input('press any key to cuntinue')
+                      os.system('clear') 
+                      os.system('python3 mac.py')
+            if u==('2') :
+                       slowprint("\033[97m")
+                       os.system('macchanger -s eth0')
+                       print(" ")
+                       allah=input('press any key to cuntinue')
+                       os.system('clear')
+                       os.system('python3 mac.py')
+
+if L==('2') :
+               print(" ")
+               print("\033[94m1 \033[97m- \033[91mwlan0 \033[97m( WIFI connexion)")
+               print("\033[94m2 \033[97m- \033[91meth0 \033[97m( CABLE connection)")
+               print(" ")
+               C=input("   \033[95m[?] \033[97menter your \033[91mconnection type \033[97m:")
+               if C==('2') :
+                        slowprint("")
+                        os.system('ifconfig eth0 down')
+                        os.system('macchanger -r eth0')
+                        os.system('ifconfig eth0 up')
+                        print(" ")
+                        hoho=input("press any key to cuntinue")
+                        os.system('clear')
+                        os.system('python3 mac.py')
+               if C==('1') :
+                        slowprint("")
+                        os.system('ifconfig wlan0 down')
+                        os.system('macchanger -r wlan0')
+                        os.system('ifconfig wlan0 up')
+                        print(" ")
+                        hoho=input("press any key to cuntinue")
+                        os.system('clear')
+                        os.system('python3 mac.py')
+
+if L==('4'): 
+               print(" ")
+               print("\033[94m1 \033[97m- \033[91mwlan0 \033[97m( WIFI connexion)")
+               print("\033[94m2 \033[97m- \033[91meth0 \033[97m( CABLE connection)")
+               print(" ")
+               i=input("   \033[95m[?] \033[97menter your \033[91mconnection type \033[97m:")
+               if i==('2') :
+                        print(" ")
+                        slowprint("\033[97m")
+                        os.system('macchanger -p eth0')
+                        print(" ") 
+                        lopa=input("press any key to cuntinue ")
+                        os.system('clear')
+                        os.system('python3 mac.py')
+               if i==('1') : 
+                        print(" ")
+                        slowprint("\033[97m")
+                        os.system('ifconfig wlan0 down')
+                        os.system('macchanger -p wlan0')
+                        os.system('ifconfig wlan0 up')
+                        print(" ") 
+                        lopa=input("press any key to cuntinue ")
+                        os.system('clear')
+                        os.system('python3 mac.py')
+
+
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(8. / 100)
+
+if L==('5'): 
+               print("\033[97m")
+               slowprint(''' 
+       [*] Static IP Assignment: Routers allow you to assign static IP addresses to your computers. When a device connects, it always receives a specific IP address if it has a matching MAC address
+
+   [*] MAC Address Filtering: Networks can use MAC address filtering, only allowing devices with specific MAC addresses to connect to a network. This isn’t a great security tool because people can spoof their MAC addresses.
+   
+   [*] MAC Authentication: Some Internet service providers may require authentication with a MAC address and only allow a device with that MAC address to connect to the Internet. You may need to change your router or computer’s MAC address to connect.
+ 
+   [*] Device Identification: Many airport Wi-Fi networks and other public Wi-Fi networks use a device’s MAC address to identify it. For example, an airport Wi-Fi network might offer a free 30 minutes and then ban your MAC address from receiving more Wi-Fi. Change your MAC address and you could get more Wi-Fi. (Free, limited Wi-Fi may also be tracked using browser cookies or an account system.)
+
+   [*] Device Tracking: Because they’re unique, MAC addresses can be used to track you. When you walk around, your smartphone scans for nearby Wi-Fi networks and broadcasts its MAC address. A company named Renew London used trash bins in the city of London to track people’s movements around the city based on their MAC addresses. Apple’s iOS 8 will use a random MAC address each time it scans for nearby Wi-Fi networks to prevent this sort of tracking. 
+                                                                                     LuCiD1774
+
+               ''')
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(3. / 100)
+
+if L==('3'):
+               print(" ")
+               print("\033[94m1 \033[97m- \033[91mwlan0 \033[97m( WIFI connexion)")
+               print("\033[94m2 \033[97m- \033[91meth0 \033[97m( CABLE connection)")
+               print(" ")
+               D=input("   \033[95m[?] \033[97menter your \033[91mconnection type \033[97m:")
+               if D==('2') :
+                        print(" ")
+                        os.system('ifconfig eth0 down')
+                        dire=input("\033[95m[?] \033[97menter THE NEW \033[92mMAC-ADDRESS \033[97m: ")
+                        os.system('ifconfig eth0 down')
+                        os.system('macchanger -m'+(dire)+' eth0')
+                        os.system('ifconfig eth0 up')
+                        print("done")
+               if D==('1') : 
+                        os.system('ifconfig wlan0 down')
+                        print(" ")
+                        dire=input("\033[95m[?] \033[97menter THE NEW \033[92mMAC-ADDRESS \033[97m: ")
+                        os.system('ifconfig wlan0 down')
+                        os.system('macchanger -m'+(dire)+' wlan0')
+                        os.system('ifconfig wlan0 up')
+                        print("done")
+
+
+
